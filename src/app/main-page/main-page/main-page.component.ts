b5c677c0333627440d8ccb5500699e4477661e9d
AnderPati <a_patino@fpzornotza.com>
2022-05-12 23:44:57 +0200

    Banner + estiloBody + desarolladores + footer

--- src/app/main-page/main-page/main-page.component.ts
+++ src/app/main-page/main-page/main-page.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  styleUrls: ['./main-page.component.scss'],
 })
 export class MainPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
