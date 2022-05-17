b3c3f9912d7b9ae7ca963887fce9d718b4997b5c
AnderPati <a_patino@fpzornotza.com>
2022-05-17 21:10:28 +0200

    contacto + scrollbar

--- src/app/main-page/main-page.module.ts
+++ src/app/main-page/main-page.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
-  declarations: [NavbarComponent, MainPageComponent],
+  declarations: [NavbarComponent, MainPageComponent, ContactoComponent],
   imports: [CommonModule],
   exports: [MainPageComponent],
 })
--- src/app/main-page/main-page/main-page.component.ts
+++ src/app/main-page/main-page/main-page.component.ts
@@ -9,5 +9,4 @@ export class MainPageComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
-
 }
