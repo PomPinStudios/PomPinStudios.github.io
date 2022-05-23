a8485bdfbb03f48ff8905121af76a0a99ae07089
AnderPati <a_patino@fpzornotza.com>
2022-05-23 19:35:18 +0200

    Noticias empezado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page/main-page.component';
+import { NoticiasComponent } from './main-page/noticias/noticias.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainPageComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'noticias',
+    component: NoticiasComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/main-page/main-page.module.ts
+++ src/app/main-page/main-page.module.ts
@@ -3,10 +3,19 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ContactoComponent } from './contacto/contacto.component';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [NavbarComponent, MainPageComponent, ContactoComponent],
-  imports: [CommonModule],
-  exports: [MainPageComponent],
+  declarations: [
+    NavbarComponent,
+    MainPageComponent,
+    ContactoComponent,
+    NoticiasComponent,
+    FooterComponent,
+  ],
+  imports: [CommonModule, RouterModule],
+  exports: [MainPageComponent, NavbarComponent, ContactoComponent, FooterComponent],
 })
 export class MainPageModule {}
