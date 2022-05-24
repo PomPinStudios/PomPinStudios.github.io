32287c7d3bee3e0895acc117e7fcdd904acb9ce2
AnderPati <a_patino@fpzornotza.com>
2022-05-24 18:55:47 +0200

    hostGit

--- src/app/main-page/noticias/noticias.component.ts
+++ src/app/main-page/noticias/noticias.component.ts
@@ -6,20 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./noticias.component.scss'],
 })
 export class NoticiasComponent implements OnInit {
-  noticias = [
+  noticias = [ //? CATEGORIAS: Interfaz, Jugabilidad, NPC, Diseño, General
     {
       titulo: '¡Estoy vivo!',
       parrafo:
-      'Es importante saber si estas a punto de morir o no, o cuanta experiencia te falta para subir de nivel, para ello se han incluido indicadores de vida y de experiencia.',
+        'Es importante saber si estas a punto de morir o no, o cuanta experiencia te falta para subir de nivel, para ello se han incluido indicadores de vida y de experiencia.',
       src: '../../../assets/vidayexp.png',
       fecha: '21/03/2022',
+      cat: 'interfaz',
     },
     {
       titulo: '¡Al ataque!',
       parrafo:
-      'Despues de que unos enemigos llegasen a nuestro juego, teniamos que dar la posibilidad de luchar contra ellos, hemos añadido y estamos trabajando en el ataque tanto de cerca como a distancia, tambien hemos hecho que los enemigos que mates te suelte experiencia para poder subir de nivel.',
+        'Despues de que unos enemigos llegasen a nuestro juego, teniamos que dar la posibilidad de luchar contra ellos, hemos añadido y estamos trabajando en el ataque tanto de cerca como a distancia, tambien hemos hecho que los enemigos que mates te suelte experiencia para poder subir de nivel.',
       src: '../../../assets/ataqueExp.gif',
       fecha: '20/03/2022',
+      cat: 'jugabilidad',
     },
     {
       titulo: '¡Mirame me muevo!',
@@ -27,6 +29,7 @@ export class NoticiasComponent implements OnInit {
         'Queriamos dar un toque de vida a los seres vivos de este mundo, pues eso hemos hecho dandoles animaciones ¡mira como se mueven estando quietos!',
       src: '../../../assets/idle.gif',
       fecha: '19/03/2022',
+      cat: 'npc',
     },
     {
       titulo: '¡A cubierto nos atacan!',
@@ -34,6 +37,7 @@ export class NoticiasComponent implements OnInit {
         'Se ha empezado a trabajan en el diseño y funcionamiento de los enemigos.',
       src: '../../../assets/enemigos.png',
       fecha: '17/03/2022',
+      cat: 'npc',
     },
     {
       titulo: '¡Ya estamos trabajando en el mapa!',
@@ -41,6 +45,7 @@ export class NoticiasComponent implements OnInit {
         'Necesitamos un terreno en el que andar, en el que vivir, que mirar, un mapa lo es todo en un juego.',
       src: '../../../assets/mapa.png',
       fecha: '08/03/2022',
+      cat: 'diseño',
     },
     {
       titulo: '¡Ya tenemos personaje!',
@@ -48,6 +53,7 @@ export class NoticiasComponent implements OnInit {
         'Ya se ha creado el peronaje principal, vamos avanzando poco a poco.',
       src: '../../../assets/pj.png',
       fecha: '03/03/2022',
+      cat: 'npc',
     },
     {
       titulo: '¡Una nueva aventura!',
@@ -55,6 +61,7 @@ export class NoticiasComponent implements OnInit {
         'Este es el inicio de un nuevo proyecto, ¡vamos a crear un nuevo juego rpg!',
       src: '../../../assets/logoRazorSinNegro.png',
       fecha: '25/02/2022',
+      cat: 'general',
     },
   ];
 
