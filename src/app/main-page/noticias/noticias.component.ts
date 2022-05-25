1d2914c4683252fb97fa438b12dd5e8fc7543856
AnderPati <a_patino@fpzornotza.com>
2022-05-25 20:24:37 +0200

    noticias acabado

--- src/app/main-page/noticias/noticias.component.ts
+++ src/app/main-page/noticias/noticias.component.ts
@@ -11,91 +11,101 @@ export class NoticiasComponent implements OnInit {
     //? CATEGORIAS: Interfaz, Jugabilidad, NPC, Diseño, Desarrollo
     {
       titulo: 'SHH!! escucha.',
-      parrafo: '¿Aquien no le gusta tener musica de fondo o escuchar increibles efectos al realizar una accion? estamos trabajando en la seccion de sonido del juego para darle mas vida y presencia a cada accion.',
-      src: '../../../assets/tavern.png',
+      parrafo:
+        '¿Aquien no le gusta tener musica de fondo o escuchar increibles efectos al realizar una accion? estamos trabajando en la seccion de sonido del juego para darle mas vida y presencia a cada accion.',
+      src: '../../../assets/altavoz.png',
       fecha: '22/04/2022',
       cat: 'Diseño',
-      color: 'border-success'
+      color: 'border-success',
     },
     {
       titulo: '¡No se me olvida!',
-      parrafo: 'No queremos que cada vez que entres al juego tengas que empezar la historia de nuevo, para ello hemos creado una opcion de guardar partida y cargar partida.',
-      src: '../../../assets/tavern.png',
+      parrafo:
+        'No queremos que cada vez que entres al juego tengas que empezar la historia de nuevo, para ello hemos creado una opcion de guardar partida y cargar partida.',
+      src: '../../../assets/guardar.png',
       fecha: '05/05/2022',
       cat: 'Jugabilidad',
-      color: 'border-danger'
+      color: 'border-danger',
     },
     {
       titulo: 'Un sitio donde vivir en paz',
-      parrafo: 'Necesitabamos un sitio principal donde transcurra la historia, ese sitio sera el pueblo principal de Razor.',
-      src: '../../../assets/tavern.png',
+      parrafo:
+        'Necesitabamos un sitio principal donde transcurra la historia, ese sitio sera el pueblo principal de Razor.',
+      src: '../../../assets/pueblo.png',
       fecha: '05/05/2022',
       cat: 'Diseño',
-      color: 'border-success'
+      color: 'border-success',
     },
     {
       titulo: '¡Vaya interiores!',
-      parrafo: 'Hace tiempo empezamos con diseños de los edificios, ahora toca darles el toque acogedor, hemos diseñado el interior de varias casas y tiendas.',
-      src: '../../../assets/tavern.png',
+      parrafo:
+        'Hace tiempo empezamos con diseños de los edificios, ahora toca darles el toque acogedor, hemos diseñado el interior de varias casas y tiendas.',
+      src: '../../../assets/interior.png',
       fecha: '19/04/2022',
       cat: 'Diseño',
-      color: 'border-success'
+      color: 'border-success',
     },
     {
       titulo: 'Un poco de curacion, que alivio.',
-      parrafo: 'Pero... ¿si me bajan la vida que hago?, hemos añadido las pociones, si te falta vida, ahora podras curarte.',
-      src: '../../../assets/tavern.png',
+      parrafo:
+        'Pero... ¿si me bajan la vida que hago?, hemos añadido las pociones, si te falta vida, ahora podras curarte.',
+      src: '../../../assets/pocionCura.png',
       fecha: '10/04/2022',
       cat: 'Jugabilidad',
-      color: 'border-danger'
+      color: 'border-danger',
     },
     {
       titulo: '¿vamos de compras?',
-      parrafo: 'Ya hemos dearrollado la mecanica de comercio, con la que podras comprar y vender objetos con los que poder interactura.',
-      src: '../../../assets/tavern.png',
+      parrafo:
+        'Ya hemos dearrollado la mecanica de comercio, con la que podras comprar y vender objetos con los que poder interactura.',
+      src: '../../../assets/tienda.png',
       fecha: '10/04/2022',
       cat: 'Jugabilidad',
-      color: 'border-danger'
+      color: 'border-danger',
     },
     {
       titulo: '¡Me lo guardo!',
-      parrafo: 'Conseguimos objetos, como por ejemplo madera, ¿pero eso donde va? se ha desarrollado un menu de inventario, para poder gestionar las bienes.',
-      src: '../../../assets/tavern.png',
+      parrafo:
+        'Conseguimos objetos, como por ejemplo madera, ¿pero eso donde va? se ha desarrollado un menu de inventario, para poder gestionar las bienes.',
+      src: '../../../assets/inventario.png',
       fecha: '07/04/2022',
       cat: 'Interfaz',
-      color: 'border-primary'
+      color: 'border-primary',
     },
     {
       titulo: '¿Hablamos un rato?',
-      parrafo: 'A quien no le gusta una buena historia? la forma en la que te sumergiras en este viaje sera tambien gracias a porder hablar con los pueblerino o leer cartas y carteles, para ello hemos desarrollado el sistema de dialogo.',
-      src: '../../../assets/tavern.png',
+      parrafo:
+        'A quien no le gusta una buena historia? la forma en la que te sumergiras en este viaje sera tambien gracias a porder hablar con los pueblerino o leer cartas y carteles, para ello hemos desarrollado el sistema de dialogo.',
+      src: '../../../assets/dialogo.gif',
       fecha: '07/04/2022',
       cat: 'Interfaz',
-      color: 'border-primary'
+      color: 'border-primary',
     },
     {
       titulo: '¡Menu!',
-      parrafo: 'Ahora puedes navegar por otras opciones gracias al menu que hemos agregado.',
-      src: '../../../assets/tavern.png',
+      parrafo:
+        'Ahora puedes navegar por otras opciones gracias al menu que hemos agregado.',
+      src: '../../../assets/menu.png',
       fecha: '04/04/2022',
       cat: 'Interfaz',
-      color: 'border-primary'
+      color: 'border-primary',
     },
     {
       titulo: '¡Ya cayo la noche!',
-      parrafo: 'Como pasa el tiempo... ya hemos desarrollado el cambio entre noche y dia, junto a un reloj que te marcara la hora actual.',
-      src: '../../../assets/tavern.png',
+      parrafo:
+        'Como pasa el tiempo... ya hemos desarrollado el cambio entre noche y dia, junto a un reloj que te marcara la hora actual.',
+      src: '../../../assets/noche.gif',
       fecha: '03/04/2022',
       cat: 'Diseño',
-      color: 'border-success'
-    }, //!--------------------------------------
+      color: 'border-success',
+    },
     {
       titulo: '¡Hogar dulce hogar!',
       parrafo: 'Ya hemos empezado con el diseño de los establecimientos.',
       src: '../../../assets/tavern.png',
       fecha: '28/03/2022',
       cat: 'Diseño',
-      color: 'border-success'
+      color: 'border-success',
     },
     {
       titulo: '¡Estoy vivo!',
@@ -104,7 +114,7 @@ export class NoticiasComponent implements OnInit {
       src: '../../../assets/vidayexp.png',
       fecha: '25/03/2022',
       cat: 'Interfaz',
-      color: 'border-primary'
+      color: 'border-primary',
     },
     {
       titulo: '¡Al ataque!',
@@ -113,7 +123,7 @@ export class NoticiasComponent implements OnInit {
       src: '../../../assets/ataqueExp.gif',
       fecha: '20/03/2022',
       cat: 'Jugabilidad',
-      color: 'border-danger'
+      color: 'border-danger',
     },
     {
       titulo: '¡Mirame me muevo!',
@@ -122,7 +132,7 @@ export class NoticiasComponent implements OnInit {
       src: '../../../assets/idle.gif',
       fecha: '19/03/2022',
       cat: 'NPC',
-      color: 'border-warning'
+      color: 'border-warning',
     },
     {
       titulo: '¡A cubierto nos atacan!',
@@ -131,7 +141,7 @@ export class NoticiasComponent implements OnInit {
       src: '../../../assets/enemigos.png',
       fecha: '17/03/2022',
       cat: 'NPC',
-      color: 'border-warning'
+      color: 'border-warning',
     },
     {
       titulo: '¡Ya estamos trabajando en el mapa!',
@@ -140,7 +150,7 @@ export class NoticiasComponent implements OnInit {
       src: '../../../assets/mapa.png',
       fecha: '08/03/2022',
       cat: 'Diseño',
-      color: 'border-success'
+      color: 'border-success',
     },
     {
       titulo: '¡Ya tenemos personaje!',
@@ -149,7 +159,7 @@ export class NoticiasComponent implements OnInit {
       src: '../../../assets/pj.png',
       fecha: '03/03/2022',
       cat: 'NPC',
-      color: 'border-warning'
+      color: 'border-warning',
     },
     {
       titulo: '¡Todo vuesto!',
@@ -158,7 +168,7 @@ export class NoticiasComponent implements OnInit {
       src: '../../../assets/GitHub.png',
       fecha: '25/02/2022',
       cat: 'Desarrollo',
-      color: 'border-info'
+      color: 'border-info',
     },
     {
       titulo: '¡Nuevo canal de YouTube!',
@@ -167,7 +177,7 @@ export class NoticiasComponent implements OnInit {
       src: '../../../assets/youtube.png',
       fecha: '25/02/2022',
       cat: 'Desarrollo',
-      color: 'border-info'
+      color: 'border-info',
     },
     {
       titulo: '¡Una nueva aventura!',
@@ -176,7 +186,7 @@ export class NoticiasComponent implements OnInit {
       src: '../../../assets/logoRazorSinNegro.png',
       fecha: '25/02/2022',
       cat: 'Desarrollo',
-      color: 'border-info'
+      color: 'border-info',
     },
   ];
 
@@ -193,9 +203,7 @@ export class NoticiasComponent implements OnInit {
       'Jugabilidad'
     ) as HTMLInputElement | null;
     const npc = document.getElementById('NPC') as HTMLInputElement | null;
-    const diseño = document.getElementById(
-      'Diseño'
-    ) as HTMLInputElement | null;
+    const diseño = document.getElementById('Diseño') as HTMLInputElement | null;
     const general = document.getElementById(
       'Desarrollo'
     ) as HTMLInputElement | null;
